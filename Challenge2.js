75dfbc56f16a4ab76450e2673efc322d626f85f9
Aai'sha Fisher <82815863+AaishaFisher@users.noreply.github.com>
2023-08-15 03:11:14 +0200

    challenge 2 complete

--- Challenge2.js
+++ Challenge2.js
@@ -1,5 +1,11 @@
 const nickName = "Timmy";
 const firstName = "Timothy";
+ 
+if (nickName) {
+    console.log("Good Morning " + nickName + "!");
+} else if (firstName) {
+    console.log("Good Morning " + firstName + "!");
+} else {
+    console.log("Good Morning!");
+}
 
-
-console.log("Good Morning," + nickName || firstName + "!");
